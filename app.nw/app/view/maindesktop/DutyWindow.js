0b73b1b3eda9e55b829d0380f92d2cf7c3ee7348
hongmomanu <hongmomanu@gmail.com>
2014-02-18 09:12:27 +0800

    jack commit

--- app.nw/app/view/maindesktop/DutyWindow.js
+++ app.nw/app/view/maindesktop/DutyWindow.js
@@ -16,9 +16,9 @@ Ext.define('Webdesktop.view.maindesktop.DutyWindow', {
         ''
     ],
 
-    //id:'grid-win',
-    alias:'widget.duty-win',
-    appType:'duty-win',
+    id:'dutywin',
+    alias:'widget.dutywin',
+    appType:'dutywin',
 
     init : function(){
         this.launcher = {
@@ -29,14 +29,16 @@ Ext.define('Webdesktop.view.maindesktop.DutyWindow', {
 
     createWindow : function(){
         var desktop = this.app.getDesktop();
-        var win = desktop.getWindow('grid-win');
+        var win = desktop.getWindow('dutywin');
         if(!win){
             win = desktop.createWindow({
-                id: 'duty-win',
+                id: 'dutywin',
                 //alias:'widget.dutywind',
                 title:'值班管理',
                 width:740,
                 height:480,
+                maximizable :true,
+                maximized:true,
                 iconCls: 'icon-duty',
                 animCollapse:false,
                 constrainHeader:true,
@@ -52,7 +54,69 @@ Ext.define('Webdesktop.view.maindesktop.DutyWindow', {
                 layout: 'fit',
                 items: [
 
-                    //Ext.widget('testgrid')
+                    {
+                        region: "center",
+                        xtype: 'tabpanel',
+                        tabBar: {
+                            hidden: true
+                        },
+
+                        items: [
+                            {
+
+                                layout: "border",
+
+
+                                items: [
+                                    {
+                                        region: 'west',
+                                        id: 'west-panel', // see Ext.getCmp() below
+                                        title: '台网值班管理',
+                                        split: false,
+                                        listeners: {
+                                            afterrender: function (panel) {
+                                                //panel.getEl().addCls('custom-header');
+                                            }
+                                        },
+                                        width: 204,
+                                        minWidth: 175,
+                                        maxWidth: 400,
+                                        collapsible: true,
+                                        animCollapse: true,
+                                        margins: '0 0 0 0',
+                                        layout: 'accordion',
+                                        items: [{
+                                            icon:'app/images/member.png',
+                                            title: '值班人员管理',
+                                            html: 'Panel content!'
+                                        },{
+                                            title: '日常工作管理',
+                                            html: 'Panel content!'
+                                        },{
+                                            title: '系统提醒管理',
+                                            html: 'Panel content!'
+                                        }]
+                                    },
+
+                                    Ext.create('Ext.tab.Panel', {
+                                        region: 'center', // a center region is ALWAYS required for border layout
+                                        //deferredRender: false,
+                                        layout:'fit',
+                                        id: 'mainContent-panel',
+                                        closeAction:'hide',
+                                        activeTab: 0,     // first tab initially active
+                                        items: [
+                                            {
+                                                title:'测试',
+                                                html: 'hello jack'
+                                            }
+                                        ]
+                                    })
+                                ]
+                            }
+                        ]
+                    }
+
                 ],
                 tbar:[{
                     text:'新增值班任务',
